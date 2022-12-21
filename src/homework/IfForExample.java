4f1d5ff6f299dfd5f691d2b77a7f1de9072b5602
dav-harutyunyan <davharutyunyan2303@gmail.com>
2022-12-21 16:39:58 +0400

    Домашная работа

---
+++ src/homework/IfForExample.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a, b;
+        a = 60;
+        b = 120;
+        if (a < b) {
+            System.out.println("max: " + b);
+            for (a = 60; a <= 120; a++) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+        a = (char) a;
+        b = (char) b;
+        System.out.println('a');
+        System.out.println('b');
+
+        if (a == b) {
+            System.out.println("a равно b");
+        } else {
+            System.out.println("a не равно b");
+
+        }
+    }
+}
