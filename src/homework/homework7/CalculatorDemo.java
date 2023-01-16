60405ec242c1d20937e8236e172be0fddfe0a957
dav-harutyunyan <davharutyunyan2303@gmail.com>
2023-01-16 19:38:22 +0400

    homework6

---
+++ src/homework/homework7/CalculatorDemo.java
@@ -0,0 +1,13 @@
+package homework.homework7;
+
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+        Calculator cl = new Calculator();
+        System.out.println(cl.plus(20,24));
+        System.out.println(cl.divide(0,4));
+        System.out.println(cl.minus(78,  76 ));
+        System.out.println(cl.multiply(65, 90));
+
+        }
+    }
