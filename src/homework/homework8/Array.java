9913b5fcdab572cbc8b1d351ca4c05bb6dd491c5
dav-harutyunyan <davharutyunyan2303@gmail.com>
2023-01-24 17:25:43 +0400

    homework8

--- src/homework/homework8/Array.java
+++ src/homework/homework8/Array.java
@@ -1,7 +1,7 @@
 package homework.homework8;
 
-public class ForExample {
+public class Array {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello");
     }
 }
